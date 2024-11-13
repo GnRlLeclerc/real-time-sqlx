a68ac9866770ad9588d1ca01c047f1db3e72f91e
Thibaut <thibaut2saivre@gmail.com>
2024-11-13 13:55:27 +0100

    Define typescript types for frontend-backend communication

--- packages/real-time-sqlx/src/types.ts
+++ packages/real-time-sqlx/src/types.ts
@@ -83,3 +83,111 @@ export interface MultipleQueryData<T> {
 
 /** Generic data return type */
 export type QueryData<T> = SingleQueryData<T> | MultipleQueryData<T>;
+
+// ************************************************************************* //
+//                                  UPDATES                                  //
+// ************************************************************************* //
+
+/** Base type for table data which has an index */
+export interface Indexable {
+  id: FinalConstraintValue;
+}
+
+/** Indexable data update type (partial attributes without the id) */
+export type UpdateData<T extends Indexable> = Partial<Omit<T, keyof Indexable>>;
+
+/** Indexable data creation type (all attributes without the id) */
+export type CreateData<T extends Indexable> = Omit<T, keyof Indexable>;
+
+/** Database operation type */
+export enum OperationType {
+  Create = "create",
+  Update = "update",
+  Delete = "delete",
+}
+
+// Granular frontend operations
+// The frontend sends these operations to be done by the backend
+
+interface GranularOperationBase {
+  type: OperationType;
+  table: string;
+}
+
+/** Create an entry in a database table */
+export interface GranularOperationCreate<T extends Indexable>
+  extends GranularOperationBase {
+  type: OperationType.Create;
+  data: CreateData<T>;
+}
+
+/** Create many entries in a database table */
+export interface GranularOperationCreateMany<T extends Indexable>
+  extends GranularOperationBase {
+  type: OperationType.Create;
+  data: CreateData<T>[];
+}
+
+/** Update an entry in a database table */
+export interface GranularOperationUpdate<T extends Indexable>
+  extends GranularOperationBase {
+  type: OperationType.Update;
+  id: FinalConstraintValue;
+  data: UpdateData<T>;
+}
+
+/** Delete an entry in a database table */
+export interface GranularOperationDelete extends GranularOperationBase {
+  type: OperationType.Delete;
+  id: FinalConstraintValue;
+}
+
+/** Granular database operation (used in the frontend) */
+export type GranularOperation =
+  | GranularOperationCreate<any>
+  | GranularOperationCreateMany<any>
+  | GranularOperationUpdate<any>
+  | GranularOperationDelete;
+
+// Complete backend operations
+// The backend sends back these operation results to be processed by the frontend
+
+interface OperationNotificationBase {
+  type: OperationType;
+  table: string;
+}
+
+/** Notification of entry creation  */
+export interface OperationNotificationCreate<T extends Indexable>
+  extends OperationNotificationBase {
+  type: OperationType.Create;
+  data: T; // The full data with ID is sent back
+}
+
+/** Notification of multiple entries creation  */
+export interface OperationNotificationCreateMany<T extends Indexable>
+  extends OperationNotificationBase {
+  type: OperationType.Create;
+  data: T[]; // The full data with ID is sent back
+}
+
+/** Notification of entry update */
+export interface OperationNotificationUpdate<T extends Indexable>
+  extends OperationNotificationBase {
+  type: OperationType.Update;
+  id: FinalConstraintValue;
+  data: T; // The full data with ID is sent back
+}
+
+/** Notification of entry deletion */
+export interface OperationNotificationDelete extends OperationNotificationBase {
+  type: OperationType.Delete;
+  id: FinalConstraintValue;
+}
+
+/** Notification of database operation (returned by the backend) */
+export type OperationNotification =
+  | OperationNotificationCreate<any>
+  | OperationNotificationCreateMany<any>
+  | OperationNotificationUpdate<any>
+  | OperationNotificationDelete;
