4896fab5db36034cd4807640f67765104a7c2f13
Thibaut <thibaut2saivre@gmail.com>
2024-11-14 18:03:49 +0100

    Finish operation test

--- crates/real-time-sqlx/src/tests/operations.rs
+++ crates/real-time-sqlx/src/tests/operations.rs
@@ -54,4 +54,78 @@ async fn test_sqlite_create() {
     }
 }
 
-// TODO : rest
+/// Test multiple row creation
+#[tokio::test]
+async fn test_sqlite_create_many() {
+    let pool = dummy_sqlite_database().await;
+    prepare_dummy_sqlite_database(&pool).await;
+
+    let operation = read_serialized_operation("02_create_many.json");
+    let result = granular_operation_sqlite(operation, &pool).await;
+
+    assert!(result.is_some());
+    let result: OperationNotification<Todo> = result.unwrap();
+
+    match result {
+        OperationNotification::CreateMany { table: _, data } => {
+            assert_eq!(data.len(), 2);
+
+            let first_data = &data[0];
+            assert_eq!(first_data.id, 4);
+            assert_eq!(first_data.title, "Fourth todo");
+            assert_eq!(first_data.content, "This is the fourth todo");
+
+            let second_data = &data[1];
+            assert_eq!(second_data.id, 5);
+            assert_eq!(second_data.title, "Fifth todo");
+            assert_eq!(second_data.content, "This is the fifth todo");
+        }
+        _ => panic!("Expected a create many operation"),
+    }
+}
+
+/// Test single row update
+#[tokio::test]
+async fn test_sqlite_update() {
+    let pool = dummy_sqlite_database().await;
+    prepare_dummy_sqlite_database(&pool).await;
+
+    let operation = read_serialized_operation("03_update.json");
+    let result = granular_operation_sqlite(operation, &pool).await;
+
+    assert!(result.is_some());
+    let result: OperationNotification<Todo> = result.unwrap();
+
+    match result {
+        OperationNotification::Update {
+            table: _,
+            id: _,
+            data,
+        } => {
+            assert_eq!(data.id, 3);
+            assert_eq!(data.title, "Updated todo");
+            assert_eq!(data.content, "This todo was updated");
+        }
+        _ => panic!("Expected an update operation"),
+    }
+}
+
+/// Test single row deletion
+#[tokio::test]
+async fn test_sqlite_delete() {
+    let pool = dummy_sqlite_database().await;
+    prepare_dummy_sqlite_database(&pool).await;
+
+    let operation = read_serialized_operation("04_delete.json");
+    let result = granular_operation_sqlite(operation, &pool).await;
+
+    assert!(result.is_some());
+    let result: OperationNotification<Todo> = result.unwrap();
+
+    match result {
+        OperationNotification::Delete { table: _, id: _ } => {
+            // Nothing to check here. The operation is not None, meaning 1 row ws affected
+        }
+        _ => panic!("Expected a delete operation"),
+    }
+}
